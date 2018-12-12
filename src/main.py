2ec7505c5c2f08438d68df191b9f2ad9b944faa1
118110081 DANIEL DE MATOS FIGUEREDO <daniel.figueredo@ccc.ufcg.edu.br>
2018-12-12 14:53:42 -0300

    Adicionei as fotos

--- src/main.py
+++ src/main.py
@@ -2,8 +2,8 @@ import cv2
 from fade import fade
 from slideshow import show
 
-imagem1 = cv2.imread("../input/daniel.jpg")
-imagem2 = cv2.imread("../input/hipo.jpg")
+imagem1 = cv2.imread("../input/eevee.jpg")
+imagem2 = cv2.imread("../input/flareon.jpg")
 
 # chamando o responsavel por realizar o fade
 fade(imagem1, imagem2)
